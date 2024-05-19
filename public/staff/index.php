7676fbeaa681e27c4dd6517c01684d30ae6c050e
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-19 11:58:52 +0545

    basice styling of staff homepage area with css

---
+++ public/staff/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GBI</title>
+    <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+</head>
+<body>
+    <header>
+        <h1>GBI Staff Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+        </ul>
+    </navigation>
+    <div id="content">
+
+    </div>
+    <footer>
+        &copy; <?php echo date('Y') ?> Globe Bank
+    </footer>
+</body>
+</html>
