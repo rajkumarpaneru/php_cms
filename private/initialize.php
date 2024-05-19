986124dce1fa480a2541e0d068e6c72b87fa2b45
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-19 16:23:24 +0545

    add ob_start() to enable output buffering

--- private/initialize.php
+++ private/initialize.php
@@ -1,5 +1,5 @@
 <?php 
-
+    ob_start(); //output buffering is turned on
     define('PRIVATE_PATH', dirname(__FILE__));
     define('PROJECT_PATH', dirname(PRIVATE_PATH));
     define('PUBLIC_PATH', PROJECT_PATH . '/public');
@@ -8,6 +8,6 @@
     $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
     $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
     define("WWW_ROOT", $doc_root);
-    
+
     require_once('functions.php');
 ?>
