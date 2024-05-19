2e57aeb089b91d693e76b2dac7b5bc94f2904c7f
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-19 12:58:14 +0545

    pass variable to include file

--- private/shared/staff_header.php
+++ private/shared/staff_header.php
@@ -1,9 +1,10 @@
+<?php if(!isset($page_title)) { $page_title = 'Staff Area'; } ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GBI</title>
+    <title>GBI - <?php echo $page_title ?> </title>
     <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
 </head>
 <body>
