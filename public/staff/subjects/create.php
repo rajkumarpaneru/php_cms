b0b67682686b57957745e5bab7135612e31ba705
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-19 20:40:08 +0545

    check request method on form submit

--- public/staff/subjects/create.php
+++ public/staff/subjects/create.php
@@ -1,7 +1,13 @@
 <?php 
-    $menu_name = $_POST['menu_name'] ?? "";
-    $position = $_POST['position'] ?? "";
-    $visible = $_POST['visible'] ?? "";
-
-    echo $menu_name;
+require_once('../../../private/initialize.php');
+    if(is_post_request()){
+        $menu_name = $_POST['menu_name'] ?? "";
+        $position = $_POST['position'] ?? "";
+        $visible = $_POST['visible'] ?? "";
+    
+        echo $menu_name;
+    } else {
+        redirect_to(url_for('/staff/subjects/new.php'));
+    }
+   
 ?>
