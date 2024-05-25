3b8a482c9c6f2529feaa81476c95ecaf71f10359
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-25 22:24:17 +0545

    get singngle record from the database

--- private/query_functions.php
+++ private/query_functions.php
@@ -16,4 +16,14 @@
         confirm_result_set($result);
         return $result;
     }
+
+    function find_subject_by_id($id){
+        global $db;
+        $sql = "SELECT * FROM subjects ";
+        $sql .= "WHERE id='". $id . "';";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        mysqli_free_result($result);
+        return $result;
+    }
 ?>
