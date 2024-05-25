6a6ebb4d3151e050f04733adb8825b022c0c15cf
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-25 14:38:54 +0545

    fetch subjects from database

--- public/staff/subjects/index.php
+++ public/staff/subjects/index.php
@@ -1,12 +1,7 @@
 <?php require_once('../../../private/initialize.php'); ?>
 
 <?php
-  $subjects = [
-    ['id' => '1', 'position' => '1', 'visible' => '1', 'menu_name' => 'About Globe Bank'],
-    ['id' => '2', 'position' => '2', 'visible' => '1', 'menu_name' => 'Consumer'],
-    ['id' => '3', 'position' => '3', 'visible' => '1', 'menu_name' => 'Small Business'],
-    ['id' => '4', 'position' => '4', 'visible' => '1', 'menu_name' => 'Commercial'],
-  ];
+  $subject_set = find_all_subjects();
 ?>
 
 <?php $page_title = 'Subjects'; ?>
@@ -43,7 +38,9 @@
     	  </tr>
       <?php } ?>
   	</table>
-
+    <?php
+      mysqli_free_result($subject_set);
+    ?>
   </div>
 
 </div>
