1327f317a26274114d7e8faa8775fc5203b73c3d
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-25 21:48:19 +0545

    fetch pages from database

--- private/query_functions.php
+++ private/query_functions.php
@@ -7,4 +7,13 @@
         confirm_result_set($result);
         return $result;
     }
+
+    function find_all_pages(){
+        global $db;
+        $sql = "SELECT * FROM pages ";
+        $sql .= "ORDER BY subject_id ASC, position ASC;";
+        $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
+        return $result;
+    }
 ?>
