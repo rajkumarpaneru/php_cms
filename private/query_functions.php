8d52547533a1b05b09fe42cf0f5b544099070c55
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-05-25 15:00:24 +0545

    handle connection and query error

--- private/query_functions.php
+++ private/query_functions.php
@@ -2,8 +2,9 @@
     function find_all_subjects(){
         global $db;
         $sql = "SELECT * FROM subjects ";
-        $sql .= "ORDER BY position ASC";
+        $sql .= "ORDER BY position ASC;";
         $result = mysqli_query($db, $sql);
+        confirm_result_set($result);
         return $result;
     }
 ?>
