81d044e8427c5c210a4ac0818984389bec2cf806
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-01 16:16:00 +0545

    make navbar collapsible

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -7,9 +7,11 @@
     <ul class="subjects">
         <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) {?>
             <li class="<?php if($nav_subject['id'] == $subject_id) { echo 'selected'; }?>">
-                <a href="<?php echo url_for('index.php'); ?>">
+                <a href="<?php echo url_for('index.php?subject_id=' . h(u($nav_subject['id']))); ?>">
                     <?php echo h($nav_subject['menu_name']); ?>
                 </a>
+
+                <?php if($nav_subject['id'] == $subject_id) {?>
                 <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
                 <ul class="subjects">
                 <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) {?>
@@ -23,6 +25,7 @@
     </ul>
     <?php mysqli_free_result($nav_pages); ?>
             </li>
+        <?php } ?>
         <?php } ?>
     </ul>
     <?php mysqli_free_result($nav_subjects); ?>
