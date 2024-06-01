c527576300fac6cca1c0745f6a28b10ede31f42f
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-01 10:28:25 +0545

    add pages for each subjects

--- private/shared/public_navigation.php
+++ private/shared/public_navigation.php
@@ -1,12 +1,25 @@
 <navigation>
-    <?php $nav_subjects = find_all_subjects() ?>
+    <?php $nav_subjects = find_all_subjects(); ?>
     <ul class="subjects">
         <?php while($nav_subject = mysqli_fetch_assoc($nav_subjects)) {?>
             <li>
                 <a href="<?php echo url_for('index.php'); ?>">
                     <?php echo h($nav_subject['menu_name']); ?>
                 </a>
+                <?php $nav_pages = find_pages_by_subject_id($nav_subject['id']); ?>
+                <ul class="subjects">
+                <?php while($nav_page = mysqli_fetch_assoc($nav_pages)) {?>
+                <li>
+                    <a href="<?php echo url_for('index.php'); ?>">
+                        <?php echo h($nav_page['menu_name']); ?>
+                    </a>
+
+                </li>
+        <?php } ?>
+    </ul>
+    <?php mysqli_free_result($nav_pages); ?>
             </li>
         <?php } ?>
     </ul>
+    <?php mysqli_free_result($nav_subjects); ?>
 </navigation>
