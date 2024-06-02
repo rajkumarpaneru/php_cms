604eb482cf3bc5fbb5b014d1e42021aa6242c243
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-02 11:35:42 +0545

    update session variables and display messages

--- private/functions.php
+++ private/functions.php
@@ -58,5 +58,20 @@ function display_errors($errors=array()) {
   return $output;
 }
 
+function display_session_message(){
+  $msg = get_add_clear_session_message();
+  if(!is_blank($msg)){
+    return '<div id="message">' . h($msg) . '</div>';
+  }
+}
+
+function get_add_clear_session_message(){
+  if(isset($_SESSION['message']) && $_SESSION['message'] != ''){
+    $msg = $_SESSION['message'];
+    unset($_SESSION['message']);
+    return $msg;
+  }
+}
+
 
 ?>
