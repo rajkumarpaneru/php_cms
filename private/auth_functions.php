fe845ab517ba83a64c2ff9e20a915ef9e13f557e
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-03 20:24:24 +0545

    implement logout

--- private/auth_functions.php
+++ private/auth_functions.php
@@ -9,6 +9,14 @@ function log_in_admin($admin){
     return true;
 }
 
+function log_out_admin(){
+    unset($_SESSION['admin_id']);
+    unset($_SESSION['last_login']);
+    unset($_SESSION['username']);
+
+    return true;
+}
+
 function is_logged_in() {
     return isset($_SESSION['admin_id']);
 }
@@ -17,7 +25,7 @@ function require_login(){
     if(!is_logged_in()){
         redirect_to(url_for('/staff/login.php'));
     }else{
-        
+
     }
 }
 
