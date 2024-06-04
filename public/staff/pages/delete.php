a1b8103936ca923229c438f55f82e3f46abc2dea
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-04 21:13:37 +0545

    nested redirect

--- public/staff/pages/delete.php
+++ public/staff/pages/delete.php
@@ -6,16 +6,15 @@ if(!isset($_GET['id'])) {
   redirect_to(url_for('/staff/pages/index.php'));
 }
 $id = $_GET['id'];
+$page = find_page_by_id($id);
+
 
 if(is_post_request()) {
 
   $result = delete_page($id);
   $_SESSION['message'] = 'The page was deleted successfully.';
-  redirect_to(url_for('/staff/pages/index.php'));
-
-} else {
-  $page = find_page_by_id($id);
-}
+  redirect_to(url_for('/staff/subjects/show.php?id='. h(u($page['subject_id']))));
+} 
 
 ?>
 
