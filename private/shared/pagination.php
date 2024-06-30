59ab1ff379556e04d4feef7fb8e5f876953bb57d
rajkumarpaneru <raajkumarpaneru@gmail.com>
2024-06-30 20:04:01 +0545

    configure previous and next button in pagination

--- private/shared/pagination.php
+++ private/shared/pagination.php
@@ -1,10 +1,24 @@
 <div class="pagination">
-  <a href="#">&laquo;</a>
+  <?php
+    if((int)$current_page > 1){
+      echo '<a href="' . url_for('/staff/subjects/index.php?perPage=10&pageNo=' . ((int)$current_page - 1)) . '">&laquo;</a>';
+    } else {
+      echo '<a href="">&laquo;</a>';
+    }
+
+  ?>
   <a href="#">1</a>
   <a href="#" class="active">2</a>
   <a href="#">3</a>
   <a href="#">4</a>
   <a href="#">5</a>
   <a href="#">6</a>
-  <a href="#">&raquo;</a>
+  <?php
+    if((int)$current_page < $total_pages){
+      echo '<a href="' . url_for('/staff/subjects/index.php?perPage=10&pageNo=' . ((int)$current_page + 1)) . '">&raquo;</a>';
+    } else {
+      echo '<a href="">&raquo;</a>';
+    }
+
+  ?>
 </div>
